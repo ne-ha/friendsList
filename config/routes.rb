cf0305b2f3f305bedfcad5f3ab9b6f7e20e62be8
NEHA SUWAl <neha@jyaasa.com>
2015-03-17 18:03:23 +0545

    Added necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  Rails.application.routes.draw do
+    root to: 'users#index', via: :get
+    get 'auth/facebook', as: "auth_provider"
+    get 'auth/facebook/callback', to: 'users#login'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
