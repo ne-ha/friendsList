6c8da9c64746991c61efa25090101b7ffa18ba01
NEHA SUWAl <neha@jyaasa.com>
2015-03-18 12:56:59 +0545

    Removed provider for google auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => 'user_friends,read_stream'}
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end
