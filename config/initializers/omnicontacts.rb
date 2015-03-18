76b6ff461190519dc2303cbf053a62f4492c4e14
NEHA SUWAl <neha@jyaasa.com>
2015-03-18 12:59:36 +0545

    Added importer for gmail

---
+++ config/initializers/omnicontacts.rb
@@ -0,0 +1,5 @@
+require "omnicontacts"
+
+Rails.application.middleware.use OmniContacts::Builder do
+  importer :gmail, "client_id", "client_secret", {:redirect_path => "/oauth2callback", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"}
+end
