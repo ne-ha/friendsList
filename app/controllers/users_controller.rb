4afad07b76e65f7a9764d78ede8d033c860d5da5
NEHA SUWAl <neha@jyaasa.com>
2015-03-18 09:22:22 +0545

    Added @friends which consits of loged in user's friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
   def login
     @token = request.env['omniauth.auth']['credentials']['token']
     @user = User.koala(request.env['omniauth.auth']['credentials'])
+    @graph = Koala::Facebook::API.new(@token)
+    @friends = @graph.get_connections("me", "friends")
   end
 end
