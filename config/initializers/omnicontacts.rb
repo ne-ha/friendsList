58af44e78570d98776d98d1e283e0318919644df
NEHA SUWAl <neha@jyaasa.com>
2015-03-18 13:18:38 +0545

    Added client id and client secret

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,5 +1,6 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, "client_id", "client_secret", {:redirect_path => "/oauth2callback", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"}
+  importer :gmail, "775333370063-v8d4p2rvcbjemhlnjt34u3vm5ekgm88g.apps.googleusercontent.com", "
+xR26XUNhcx6QnGJv8wgl7Zfl", {:redirect_path => "/oauth2callback", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"}
 end
