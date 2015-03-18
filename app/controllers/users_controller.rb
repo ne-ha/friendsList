74ff24c8448db3dd4a7926c12ee834287190531b
NEHA SUWAl <neha@jyaasa.com>
2015-03-18 09:18:21 +0545

    @token consist of loged in user's token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   end
 
   def login
+    @token = request.env['omniauth.auth']['credentials']['token']
     @user = User.koala(request.env['omniauth.auth']['credentials'])
   end
 end
