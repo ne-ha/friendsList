40a641f490e32410ea88525b7da5fe3bfab88bcd
NEHA SUWAl <neha@jyaasa.com>
2015-03-20 17:28:25 +0545

    Added action callback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,15 @@ class UsersController < ApplicationController
     render "index"
   end
 
+  def callback
+    @contacts = request.env['omnicontacts.contacts']
+    @user = request.env['omnicontacts.user']
+    puts "List of contacts of #{@user[:name]} obtained from #{params[:importer]}:"
+    @contacts.each do |contact|
+      puts "Contact found: name => #{contact[:name]}, email => #{contact[:email]}"
+    end
+  end
+
   def failure
   end
 
