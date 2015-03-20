e3ccef51b8ccee4e4aaafef8649bd0c58607295d
NEHA SUWAl <neha@jyaasa.com>
2015-03-20 10:40:30 +0545

    Changed the app_id and secret for develop

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -2,6 +2,6 @@ require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
   importer :gmail, 
-  "775333370063-c4ba2sll69vqrm9bjml3nvt6agjvb9n4.apps.googleusercontent.com", 
-  "EKWhRNKudLqf30FdOy12gNvm"
+  "775333370063-lc2d7dpgmoohfv95anhtm5iaubhlfkra.apps.googleusercontent.com", 
+  "ZWR1BRVG_Xs3AW-IGGbJase7"
 end
