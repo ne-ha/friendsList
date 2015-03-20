bf1c2511a70492aeb2af45fb099251d3d0a48dae
NEHA SUWAl <neha@jyaasa.com>
2015-03-20 17:09:52 +0545

    Manged the actions index and contact_info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,30 +2,29 @@ class UsersController < ApplicationController
   require 'koala'
 
   def index
-    contacts_info
-    @contacts = Kaminari.paginate_array(session['name_email']).page(params[:page]).per(3)
+    if session['name_email'].present?
+      @contacts = Kaminari.paginate_array(session['name_email']).page(params[:page]).per(10)
+    else
+      @contacts = nil
+    end
     respond_to do |format|
       format.html
     end
   end
 
   def contacts_info
-    @all_contacts = request.env['omnicontacts.contacts']
-    if @all_contacts.nil?
-      return nil
+    @contacts = request.env['omnicontacts.contacts']
+    if @contacts.nil?
+      session['name_email'] = nil
     else
-      session['name_email'] = @all_contacts.map{|c| [c[:name], c[:email]] }
-      return session['name_email']
+      session['name_email'] = @contacts.map{|c| [c[:name], c[:email]] }
     end
+    redirect_to :action=>'index'
   end
 
-  def callback
-    @contacts = request.env['omnicontacts.contacts']
-    @user = request.env['omnicontacts.user']
-    puts "List of contacts of #{@user[:name]} obtained from #{params[:importer]}:"
-    @contacts.each do |contact|
-      puts "Contact found: name => #{contact[:name]}, email => #{contact[:email]}"
-    end
+  def logout
+    session['name_email'] = nil
+    render "index"
   end
 
   def failure
